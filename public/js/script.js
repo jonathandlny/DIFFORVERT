27a10e8f65b73c69e70c495fa33c94913331fe4f
Jonathan <contact.informatique@ribegroupe.com>
2020-01-22 18:35:30 +0100

    I DON'T KNOW

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('select').formSelect();
-
-    var sH = $(window).height();
-    var login = $('.login').height();
-    $('.login').css('transform','translateY(' + login/2 + 'px)');
 });
